4ee2916afa588f7d3f2a647119c341a7a3ca9768
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-22 06:55:26 +0530

    Update terraform/terraform-vpc/

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "vpc" {
-  source   = "./vendor/modules/vpc"
-  VPC_CIDR = var.VPC_CIDR
-  ENV      = var.ENV
+  source      = "./vendor/modules/vpc"
+  VPC_CIDR    = var.VPC_CIDR
+  ENV         = var.ENV
+  SUBNET_CIDR = var.SUBNET_CIDR
+  AZ          = var.AZ
 }
 
