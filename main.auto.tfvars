96a8e7d70168ef7b4ffaca66b8d5dff78394a6b4
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-22 07:23:37 +0530

    Update terraform/terraform-vpc/

--- main.auto.tfvars
+++ main.auto.tfvars
@@ -1,2 +1,3 @@
-AZ             = ["us-east-1a", "us-east-1b"]
-DEFAULT_VPC_ID = "vpc-0b9acf68cfad58fff"
+AZ               = ["us-east-1a", "us-east-1b"]
+DEFAULT_VPC_ID   = "vpc-0b9acf68cfad58fff"
+DEFAULT_VPC_CIDR = "172.31.0.0/16"
--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 module "vpc" {
-  source         = "./vendor/modules/vpc"
-  VPC_CIDR       = var.VPC_CIDR
-  ENV            = var.ENV
-  SUBNET_CIDR    = var.SUBNET_CIDR
-  AZ             = var.AZ
-  DEFAULT_VPC_ID = var.DEFAULT_VPC_ID
+  source           = "./vendor/modules/vpc"
+  VPC_CIDR         = var.VPC_CIDR
+  ENV              = var.ENV
+  SUBNET_CIDR      = var.SUBNET_CIDR
+  AZ               = var.AZ
+  DEFAULT_VPC_ID   = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
 }
 
