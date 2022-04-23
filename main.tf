a857dc64190ba6af60f3efc8528e4d20832a4fd8
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-23 06:55:21 +0530

    Update terraform/terraform-vpc/

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "vpc" {
-  source           = "./vendor/modules/vpc"
-  VPC_CIDR         = var.VPC_CIDR
-  ENV              = var.ENV
-  SUBNET_CIDR      = var.SUBNET_CIDR
-  AZ               = var.AZ
-  DEFAULT_VPC_ID   = var.DEFAULT_VPC_ID
-  DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
-  DEFAULT_VPC_RT   = var.DEFAULT_VPC_RT
+  source              = "./vendor/modules/vpc"
+  VPC_CIDR            = var.VPC_CIDR
+  ENV                 = var.ENV
+  PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  AZ                  = var.AZ
+  DEFAULT_VPC_ID      = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_CIDR    = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_RT      = var.DEFAULT_VPC_RT
 }
 
